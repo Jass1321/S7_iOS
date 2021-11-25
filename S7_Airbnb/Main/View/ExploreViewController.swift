b51125663549cb8c75b010a72c0f9ca0f315ad87
Jass1321 <jassyra13216@gmail.com>
2021-11-25 11:39:46 -0500

    tableview & cell dynamic

--- S7_Airbnb/Main/View/ExploreViewController.swift
+++ S7_Airbnb/Main/View/ExploreViewController.swift
@@ -1,3 +1,4 @@
+
 //
 //  ExplorerViewController.swift
 //  S7_Airbnb
@@ -8,57 +9,67 @@
 import UIKit
 
 class ExploreViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     
-    //Creae arreglo de lugares
-    let places: [String] = ["Mancora","Acapulco","Miami","Cancun"]
-    let datesToTravel: [String] = ["4-6 Nov","11-14 Dic","1-5 Ene","3-6 Feb"]
-    
+    let placeViewModel = PlaceViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTable()
+        configure()
+        bind()
     }
            
    func setUpTable() {
        tableView.delegate = self
        tableView.dataSource = self
    }
+    
+    func configure() {
+           placeViewModel.getPlaces()
+   }
+   
+   func bind() {
+       placeViewModel.refreshData = { [weak self] () in
+           DispatchQueue.main.async {
+               self?.tableView.reloadData()
+           }
+       }
+   }
 }
 
 extension ExploreViewController:  UITableViewDelegate, UITableViewDataSource {
    
    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-       return places.count
+       return placeViewModel.arrayResults.count
    }
    
    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
        let cell = tableView.dequeueReusableCell(withIdentifier: "cellExplore", for: indexPath) as! ExploreTableViewCell
-       
-       cell.lblTitle.text = places[indexPath.row]
-       cell.lblAddress.text = datesToTravel[indexPath.row]
-       
-       if indexPath.row == 0 {
-           cell.imageExplore?.image = UIImage(named: "mancora")
-       } else if indexPath.row == 1 {
-           cell.imageExplore?.image = UIImage(named: "acapulco")
-       } else if indexPath.row == 2 {
-           cell.imageExplore?.image = UIImage(named: "miami")
-       } else {
-           cell.imageExplore?.image = UIImage(named: "canun")
-       }
-       
+               
+       let object = placeViewModel.arrayResults[indexPath.row]
+              
+      cell.lblTitle.text = object.name
+      cell.lblAddress.text = object.address
+      cell.lblPrice.text = "S/.120.00"
+      cell.lblRating.text = String(object.rating)
+      cell.lblCountRating.text = "(\(object.userRatingsTotal))"
       
-       cell.lblPrice.text = "$120.00 / night"
-       cell.lblRating.text = "4.69"
-       cell.lblCountRating.text = "(120)"
-       
-       let cellView = UIView()
-       cellView.backgroundColor = UIColor.systemBackground
-       cell.selectedBackgroundView = cellView
-       
-       return cell
+      let urlImage = URL(string: object.photo)
+  
+      let data = try? Data(contentsOf: urlImage!)
+      
+      if let imageData = data {
+          cell.imageExplore?.image = UIImage(data: imageData)
+      }
+      //no se sombree
+      let cellView = UIView()
+      cellView.backgroundColor = UIColor.systemBackground
+      cell.selectedBackgroundView = cellView
+      
+      return cell
    }
         
 }
